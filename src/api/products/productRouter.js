9cfabd6523598056dcf484335c540ce7b55e8174
Ignacio Gandolfo <nachingast@gmail.com>
2023-03-27 13:24:59 -0300

    productRouter actualizado con los try y catch en los endpoints

--- src/api/products/productRouter.js
+++ src/api/products/productRouter.js
@@ -6,83 +6,107 @@ const router = Router();
 const manager = new ProductManager();
 
 router.get("/", async (req, res) => {
-  const { limit } = req.query;
+  try {
+    const { limit } = req.query;
+    const products = await manager.getProducts();
 
-  const products = await manager.getProducts();
+    if (!limit) {
+      return res.send({ products: products });
+    }
 
-  if (!limit) {
-    return res.send({ products: products });
+    const limitedProducts = products.slice(0, limit);
+
+    return res.send({ products: limitedProducts });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send({ error: "Internal Server Error" });
   }
-
-  const limitedProducts = products.slice(0, limit);
-
-  return res.send({ products: limitedProducts });
 });
 
 router.get("/:pid", async (req, res) => {
-  const productId = req.params.pid;
-  const product = await manager.getProductById(+productId);
+  try {
+    const productId = req.params.pid;
+    const product = await manager.getProductById(+productId);
 
-  if (!product) {
-    return res
-      .status(404)
-      .send({ status: "Error", error: "product was not found" });
+    if (!product) {
+      return res
+        .status(404)
+        .send({ status: "Error", error: "product was not found" });
+    }
+    return res.send({ status: "OK", message: product });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send({ error: "Internal Server Error" });
   }
-  return res.send({ status: "OK", message: product });
 });
 
 router.post("/", uploader.array("thumbnails", 5), async (req, res) => {
-  const product = req.body;
-  const files = req.files;
+  try {
+    const product = req.body;
+    const files = req.files;
 
-  if (!product) {
-    return res.status(400).send({
-      status: "Error",
-      error: "Error, cannot find the product",
-    });
+    if (!product) {
+      return res.status(400).send({
+        status: "Error",
+        error: "Error, cannot find the product",
+      });
+    }
+
+    product.thumbnails = [];
+
+    if (files) {
+      files.forEach((file) => {
+        const imageUrl = `http://localhost:8080/images/${file.thumbnail}`;
+        product.thumbnails.push(imageUrl);
+      });
+    }
+
+    await manager.addProduct(product);
+    return res.send({ status: "OK", message: "product successfully added" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send({ error: "Internal Server Error" });
   }
-
-  product.thumbnails = [];
-
-  if (files) {
-    files.forEach((file) => {
-      const imageUrl = `http://localhost:8080/images/${file.thumbnail}`;
-      product.thumbnails.push(imageUrl);
-    });
-  }
-
-  await manager.addProduct(product);
-  return res.send({ status: "OK", message: "product successfully added" });
 });
 
 router.put("/:pid", async (req, res) => {
-  const productId = req.params.pid;
-  const changes = req.body;
+  try {
+    const productId = req.params.pid;
+    const changes = req.body;
 
-  const updatedProduct = await manager.updateProduct(+productId, changes);
+    const updatedProduct = await manager.updateProduct(+productId, changes);
 
-  if (!updatedProduct) {
-    return res
-      .status(404)
-      .send({ statud: "Error", error: "Product was not found" });
+    if (!updatedProduct) {
+      return res
+        .status(404)
+        .send({ statud: "Error", error: "Product was not found" });
+    }
+    return res.send({
+      status: "OK",
+      message: "Product succesfully updated",
+      product: updatedProduct,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send({ error: "Internal Server Error" });
   }
-  return res.send({
-    status: "OK",
-    message: "Product succesfully updated",
-    product: updatedProduct,
-  });
 });
 
 router.delete("/:pid", async (req, res) => {
-  const productId = req.params.pid;
-  const deletedProduct = await manager.deleteProduct(+productId);
+  try {
+    const productId = req.params.pid;
+    const deletedProduct = await manager.deleteProduct(+productId);
 
-  if (!deletedProduct) {
-    return res
-      .status(404)
-      .send({ status: "Error", error: "Product does not exist" });
+    if (!deletedProduct) {
+      return res
+        .status(404)
+        .send({ status: "Error", error: "Product does not exist" });
+    }
+    return res.send({ status: "OK", message: "PRoduct deleted successfully" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send({ error: "Internal Server Error" });
   }
-  return res.send({ status: "OK", message: "PRoduct deleted successfully" });
 });
 
 export default router;
